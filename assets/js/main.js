4d8619a43ad351706ee9b23d4063bc13436669db
garrettscreations <31220746+garrettscreations@users.noreply.github.com>
2017-08-23 22:14:41 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 /*
-	Epilogue by TEMPLATED
+	Projection by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
 (function($) {
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
-	});
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
 
 	$(function() {
 
@@ -29,9 +29,6 @@
 				}, 100);
 			});
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
 		// Prioritize "important" elements on medium.
 			skel.on('+medium -medium', function() {
 				$.prioritize(
@@ -40,21 +37,29 @@
 				);
 			});
 
-		// Items.
-			$('.item').each(function() {
+	// Off-Canvas Navigation.
 
-				var $this = $(this),
-					$header = $this.find('header'),
-					$a = $header.find('a'),
-					$img = $header.find('img');
+		// Navigation Panel.
+			$(
+				'<div id="navPanel">' +
+					$('#nav').html() +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left'
+				});
 
-				// Set background.
-					$a.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Remove original image.
-					$img.remove();
-
-			});
+		// Fix: Remove transitions on WP<10 (poor/buggy performance).
+			if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+				$('#navPanel')
+					.css('transition', 'none');
 
 	});
 
